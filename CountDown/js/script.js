960f3cc8c1eb8f0dcb70ff5763025715bb3ed374
Rahul Rai <protocolpsi@gmail.com>
2025-08-04 08:08:52 +0530

    first commit

---
+++ CountDown/js/script.js
@@ -0,0 +1,27 @@
+const endDate = "20 August 2025 10:00 PM "
+document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input");
+// const clock = () => {
+
+// }
+
+function clock(){
+    const end = new Date(endDate);
+    const now = new Date();
+    const diff = (end - now)/1000;
+    if(diff < 0) return;
+    inputs[0].value = Math.floor(diff / 3600 / 24);
+    inputs[1].value = Math.floor(diff /3600)%24;
+    inputs[2].value = Math.floor((diff/60)%60)
+    inputs[3].value = Math.floor(diff%60);
+
+    // convert into days
+
+}
+clock()
+
+setInterval(
+    () =>{
+        clock()
+    }
+)
